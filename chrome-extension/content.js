117c98b6748c57d1373de0a090be333b411c1549
YuugouOhno <yuugou.ohno@gmail.com>
2023-10-02 09:32:51 +0900

    画面を覆うモーダルを作成、表示

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,1 +1,17 @@
 window.alert('YouTube見るつもりか!?');
+
+document.body.innerHTML += `
+    <div id="myModal" class="modal">
+        <p id="closeButton" class="close">&times;</p>
+    </div>`;
+
+var modal = document.getElementById("myModal");
+var closeButton = document.getElementById("closeButton");
+
+modal.style.display = "block";
+
+window.onclick = function (event) {
+    if (event.target == closeButton) {
+        modal.style.display = "none";
+    }
+}
