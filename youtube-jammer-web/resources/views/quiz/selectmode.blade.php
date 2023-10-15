953541474515e597e7266f857c2fb2d026eb7cd2
okkunn139 <okujuny@gmail.com>
2023-10-15 13:11:27 +0900

    selectmodeのフロント編集

--- youtube-jammer-web/resources/views/quiz/selectmode.blade.php
+++ youtube-jammer-web/resources/views/quiz/selectmode.blade.php
@@ -9,22 +9,46 @@
       <div id="quizcontainer">
           @if (session('mode') && session('rank'))
           <div class="alert alert-success">
-                  <h2 class="text-8xl font-sans">ランキング</h2>
-                  <p>{{ session('mode') }}</p>
-                  <p>{{ session('rank') }}位</p>
+                <br><br>
+                  <h2 class="text-4xl font-sans text-center text-gray-100">
+                    Ranking(問題数別)
+                  </h2>
+                  <!-- <p>{{ session('mode') }}</p> -->
+                  <p class="text-center">
+                    <span class="text-2xl font-sans text-gray-100">
+                        あなたの順位は… 
+                    </span>
+                    <span class="text-5xl font-sans text-gray-100 underline">
+                        {{ session('rank') }}位
+                    </span>
+                  </p>
               </div>
           @endif
           <div class="flex justify-center items-center text-center mt-8">
             <div class="columns-1">
-                <p class="text-6xl">挑戦</p>
-                <p class="text-2xl">モード選択</p>
-                <div class="mb-10">
-                    <a href="{{ route('quiz.index', ['mode' => 1]) }}">問題数1</a>
-                    <a href="{{ route('quiz.index', ['mode' => 2]) }}">問題数2</a>
-                    <a href="{{ route('quiz.index', ['mode' => 3]) }}">問題数3</a>
+                <br>
+                <p class="text-3xl text-gray-100 underline font-bold">単語和訳クイズに挑戦！</p>
+                <br>
+                <br>
+                <p class="text-2xl text-gray-100">問題数の選択</p>
+                <br>
+                <div class="mb-10 text-gray-100">
+                    <a href="{{ route('quiz.index', ['mode' => 1]) }}">
+                        <button class="bg-gradient-to-b from-red-300 to-red-800 hover:bg-gradient-to-l text-white rounded px-4 py-4 text-2xl font-bold">問題数<br>10</button>
+                    </a>
+                    <a href="{{ route('quiz.index', ['mode' => 2]) }}">
+                        <button class="bg-gradient-to-b from-green-300 to-green-800 hover:bg-gradient-to-l text-white rounded px-4 py-4 text-2xl font-bold">問題数<br>50</button>
+                    </a>
+                    <a href="{{ route('quiz.index', ['mode' => 3]) }}">
+                        <button class="bg-gradient-to-b from-blue-300 to-blue-800 hover:bg-gradient-to-l text-white rounded px-4 py-4 text-2xl font-bold">問題数<br>100</button>
+                    </a>
                 </div>
-                <p class="text-6xl">レベル上げ</p>
-                <a href="{{ route('card')}}">単語帳へ</a>
+                <br>
+                <p class="text-2xl text-gray-100">※単語帳で勉強したい方はコチラ</p>
+                <br>
+                <a href="{{ route('card')}}">
+                    <button class="bg-gradient-to-r from-yellow-100 to-yellow-400 hover:bg-gradient-to-l text-black rounded px-4 py-2 text-2xl font-bold">単語帳</button>
+                </a>
             </div>
           </div>
       </div>
